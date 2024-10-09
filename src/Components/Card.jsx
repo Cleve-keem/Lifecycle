437c67875dd2e9c68622d1197d70bd4c1ad16e32
hakeem <belloharyourmidey@gmail.com>
2024-10-08 23:00:22 -0700

    rebuilding

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,62 +1,57 @@
-import './Card.modulus.css'
-import { useState, useEffect, useRef } from 'react'
+import './Card.modulus.css';
+import { useState, useEffect, useRef } from 'react';
 
-const colors = ["purple", "grey", "lightblue"];
+const colors = ["purple", "tomato", "red", "blue", "orange"];
 
 const Card = () => {
+    const [ showPara, setShowPara ] = useState(true); // state to manage the paragraph changes
+    const [ color, setColor ] = useState(colors[0]);
 
+    const isMounted = useRef(false);
 
-    const [ showText, setShowText] = useState(true);
-    const [ color, setColor] = useState(colors[0]);
-    const isMountedRef = useRef(false);
-
-    const randomizeArr = (arr) => {
-        const cloned = [...arr];
-        for(let i = cloned.length - 1; i > 0; i--){
-            const j = Math.floor(Math.random() * (i+1))
-            ;[cloned[i], cloned[j]] = [cloned[j], cloned[i]] 
-        }
-
-        return cloned
-    }
-
-    useEffect(() => {
-        if(!isMountedRef.current){
-            console.log("i am born") // do something before you render template or something.
-            isMountedRef.current = true
+    useEffect(()=> {
+        if(!isMounted.current){
+            console.log("i am born");
+            isMounted.current = true;
         }
         else{
-            console.log("i am updated") // do something when it is updated
+            console.log("i am updated!")
         }
 
-        return () => {
-            console.log('cleaning up')  // do something before it leaves. maybe stop execution
+        return ()=> {
+            console.log('i am cleaning')
         }
     });
 
-    return(
-        <div className="container">
+    const randomizeArr = array => {
+        const copyArr = [...array]
+        for(let i = copyArr.length - 1; i > 0; i--){
+            const j = Math.floor(Math.random() * ( i + 1))
+            ;[copyArr[i], copyArr[j]] = [copyArr[j], copyArr[i]]
+        }
+
+        return copyArr;
+    }
+
+    return (
+        <div className='container'>
             <div style={{
                 background: color
-            }} className="card_wrapper">
+            }} className="card-wrapper">
                 <div className="card">
-                    <h1>This is a business card</h1>
-                    {showText && <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet magni commodi ipsa dolor, quo quam sit. Autem, quasi nobis quae quidem necessitatibus iusto sunt blanditiis nulla? Aspernatur voluptatem temporibus odio?</p>}
+                    <h1>This is a Card</h1>
+                    {showPara && <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A animi pariatur praesentium numquam dolor ratione repudiandae placeat totam vitae magni, vero reiciendis nesciunt. Temporibus obcaecati, dignissimos optio aperiam distinctio rem.</p>}
+                    
                     <button
-                     onClick={ () => {
-                        setShowText(!showText)
-                     }
-                    }>
-                        Show text
-                    </button>
-                    <button
-                     onClick={ () => {
-                        const randomColor = randomizeArr(colors)[0];
-                        setColor(randomColor);
-                     }
-                    }>
-                        Change Background Color
-                    </button>
+                        onClick={ () => {
+                            setShowPara(!showPara)
+                        }}
+                    >Show Paragraph</button>
+                    <button onClick={()=> {
+                        const randomColor = randomizeArr(colors)[0]
+                        setColor(randomColor)
+                        console.log(randomColor);
+                    }}>Change Background Color</button>
                 </div>
             </div>
         </div>
